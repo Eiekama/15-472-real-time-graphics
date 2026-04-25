ffad33d71b795b7f0f18b873986a5ba5d2ee1429
Eiekama <42797046+Eiekama@users.noreply.github.com>
2026-04-24 23:49:57 -0400

    final report done

--- python_helpers/run_tests.sh
+++ python_helpers/run_tests.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
-# for i in {1,2,5,10,20,50,100,200,500,1000,2000,5000}; do
-#     ./bin/viewer --scene ../a3-submission/report/s72-files/lights-performance-test/sun_$i.s72 --timer ../a3-submission/report/timings/sun-lights/sun_$i.txt --headless < _temp.txt 
-# done
-# for i in {1,2,5,10,20,50,100,200,500,1000,2000,5000}; do
-#     ./bin/viewer --scene ../a3-submission/report/s72-files/lights-performance-test/sphere_$i.s72 --timer ../a3-submission/report/timings/sphere-lights/sphere_$i.txt --headless < _temp.txt
-# done
-# for i in {1,2,5,10,20,50,100,200,500,1000,2000,5000}; do
-#     ./bin/viewer --scene ../a3-submission/report/s72-files/lights-performance-test/spot_$i.s72 --timer ../a3-submission/report/timings/spot-lights/spot_$i.txt --headless < _temp.txt 
-# done
 
-python ../python_helpers/analyze.py ../a3-submission/report/timings/sun-lights/
-python ../python_helpers/analyze.py ../a3-submission/report/timings/sphere-lights/
-python ../python_helpers/analyze.py ../a3-submission/report/timings/spot-lights/
+# source /Users/chenjingxuan/VulkanSDK/1.4.335.1/setup-env.sh  
+
+# ./bin/viewer --scene ../f-submission/report/scenes/cloud.s72 --headless --timer ../f-submission/report/timings/step_size/adj250.txt < _temp.txt
+# ./bin/viewer --scene ../f-submission/report/scenes/cloud_big.s72 --headless --timer ../f-submission/report/timings/cloud_big_no_adastep.txt < _temp.txt
+# ./bin/viewer --scene ../f-submission/report/scenes/cloud_16.s72 --headless --timer ../f-submission/report/timings/cloud_16.txt < _temp.txt
+# ./bin/viewer --scene ../f-submission/report/scenes/cloud_32.s72 --headless --timer ../f-submission/report/timings/cloud_32.txt < _temp.txt
+# ./bin/viewer --scene ../f-submission/report/scenes/cloud_64.s72 --headless --timer ../f-submission/report/timings/cloud_64.txt < _temp.txt
+
+
+# python ../python_helpers/analyze.py ../f-submission/report/timings/screen_space/cloud_small.txt -r
+# python ../python_helpers/analyze.py ../f-submission/report/timings/screen_space/cloud_small_no_adastep.txt -r
+# python ../python_helpers/analyze.py ../f-submission/report/timings/screen_space/cloud_big.txt -r
+# python ../python_helpers/analyze.py ../f-submission/report/timings/screen_space/cloud_big_no_adastep.txt -r
+
+python ../python_helpers/analyze.py ../f-submission/report/timings/step_size/near/
+python ../python_helpers/analyze.py ../f-submission/report/timings/step_size/offset/
+python ../python_helpers/analyze.py ../f-submission/report/timings/step_size/adj/
